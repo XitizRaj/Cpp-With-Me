d9207240e490549b391cca72ecb2dccadba44066
XitizRaj <xitizraj2004@gmail.com>
2025-12-27 19:27:46 +0530

    Add files via upload

---
+++ FloatData.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a = 10; // dividend
+    int b = 3; // divisior
+    int q = a/b; // quotient
+    int r = a - (b*q); //remainder
+    cout<<r;
+}
